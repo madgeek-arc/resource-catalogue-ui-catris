69ca6f42422b3c593c2f4dbbe317a5120ff135ff
amantas <andreas_mantas@hotmail.com>
2020-10-29 12:33:17 +0200

    added environment.beta.ts

---
+++ src/environments/environment.beta.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: false,
+  beta: true,
+  FAQ_ENDPOINT: '/faq/api',
+  API_ENDPOINT: '/api',
+  API_TOKEN_ENDPOINT: 'https://aai.eosc-portal.eu/eic-api/token.php',
+  projectName: 'CatRIS',
+  projectMail: 'support@catris.eu',
+  serviceORresource: 'Service',
+  hasUserConsent: true,
+  privacyPolicyURL: 'https://www.portal.catris.eu/support/privacy-policy',
+  marketplaceBaseURL: ''
+};
