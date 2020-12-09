aef9afdbf6e6f0ea40d60f84e55834f59498e424
jBalasis <cs2180008@di.uoa.gr>
2020-12-09 11:37:20 +0200

    git subrepo pull src/lib
    subrepo:
      subdir:   "src/lib"
      merged:   "0fbc698"
    upstream:
      origin:   "https://github.com/madgeek-arc/resource-catalogue-lib.git"
      branch:   "master"
      commit:   "0fbc698"
    git-subrepo:
      version:  "0.4.1"
      origin:   "https://github.com/ingydotnet/git-subrepo"
      commit:   "a04d8c2"

--- src/lib/pages/admin/resources-list.component.ts
+++ src/lib/pages/admin/resources-list.component.ts
@@ -2,13 +2,15 @@ import {Component, OnInit} from '@angular/core';
 import {ResourceService} from '../../services/resource.service';
 import {ServiceProviderService} from '../../services/service-provider.service';
 import {statusChangeMap, statusList} from '../../domain/service-provider-status-list';
-import {InfraService, ProviderBundle, ServiceBundle} from '../../domain/eic-model';
+import {InfraService, ProviderBundle} from '../../domain/eic-model';
 import {environment} from '../../../environments/environment';
 import {mergeMap} from 'rxjs/operators';
 import {AuthenticationService} from '../../services/authentication.service';
 import {ActivatedRoute, Router} from '@angular/router';
 import {FormArray, FormBuilder, FormControl, FormGroup} from '@angular/forms';
 import {URLParameter} from '../../domain/url-parameter';
+import {NavigationService} from '../../services/navigation.service';
+import {PremiumSortFacetsPipe} from '../../shared/pipes/premium-sort.pipe';
 
 declare var UIkit: any;
 
@@ -26,9 +28,8 @@ export class ResourcesListComponent implements OnInit {
     order: 'ASC',
     quantity: '10',
     from: '0',
-    // status: new FormArray([]),
-    status: '',
-    active: ''
+    active: '',
+    resource_organisation: new FormArray([])
   };
 
   dataForm: FormGroup;
@@ -38,12 +39,13 @@ export class ResourcesListComponent implements OnInit {
   errorMessage: string;
   loadingMessage = '';
 
-  services: ServiceBundle[] = [];
-  selectedService: ServiceBundle;
-  facets: any;
-
   providers: ProviderBundle[] = [];
   selectedProvider: ProviderBundle;
+  providersTotal: number;
+
+  services: InfraService[] = [];
+  selectedService: InfraService;
+  facets: any;
 
   total: number;
   // from = 0;
@@ -59,6 +61,7 @@ export class ResourcesListComponent implements OnInit {
               private authenticationService: AuthenticationService,
               private route: ActivatedRoute,
               private router: Router,
+              private navigator: NavigationService,
               private fb: FormBuilder
   ) {
   }
@@ -73,7 +76,34 @@ export class ResourcesListComponent implements OnInit {
       this.route.queryParams
         .subscribe(params => {
 
+            for (const i in params) {
+              if (i === 'resource_organisation') {
+
+                if (this.dataForm.get('resource_organisation').value.length === 0) {
+                  const formArrayNew: FormArray = this.dataForm.get('resource_organisation') as FormArray;
+                  // formArrayNew = this.fb.array([]);
+                  for (const resource_organisation of params[i].split(',')) {
+                    if (resource_organisation !== '') {
+                      formArrayNew.push(new FormControl(resource_organisation));
+                    }
+                  }
+                }
+              } else {
+                this.dataForm.get(i).setValue(params[i]);
+              }
+            }
+
+            for (const i in this.dataForm.controls) {
+              if (this.dataForm.get(i).value) {
+                const urlParam = new URLParameter();
+                urlParam.key = i;
+                urlParam.values = [this.dataForm.get(i).value];
+                this.urlParams.push(urlParam);
+              }
+            }
+
             this.getServices();
+            this.getProviders();
             // this.handleChange();
           },
           error => this.errorMessage = <any>error
@@ -111,6 +141,7 @@ export class ResourcesListComponent implements OnInit {
       map[urlParameter.key] = concatValue;
     }
 
+    // console.log('map', map);
     this.router.navigate([`/provider/resource/all`], {queryParams: map});
     // this.getServices();
   }
@@ -120,11 +151,27 @@ export class ResourcesListComponent implements OnInit {
     this.handleChange();
   }
 
+  getProviders() {
+    this.providers = [];
+    this.resourceService.getProviderBundles('0', '1000', 'name', 'ASC', '', []).subscribe(
+      res => {
+        this.providers = res['results'];
+        this.providersTotal = res['total'];
+      },
+      err => {
+        console.log(err);
+        this.errorMessage = 'The list could not be retrieved';
+      },
+      () => {
+      }
+    );
+  }
+
   getServices() {
     this.services = [];
     this.resourceService.getResourceBundles(this.dataForm.get('from').value, this.dataForm.get('quantity').value,
       this.dataForm.get('orderField').value, this.dataForm.get('order').value, this.dataForm.get('query').value,
-      this.dataForm.get('active').value).subscribe(
+      this.dataForm.get('active').value, this.dataForm.get('resource_organisation').value).subscribe(
       res => {
         this.services = res['results'];
         this.facets = res['facets'];
@@ -136,25 +183,105 @@ export class ResourcesListComponent implements OnInit {
         this.errorMessage = 'The list could not be retrieved';
       },
       () => {
-        // this.services.forEach(
-        //   p => {
-        //     if ((p.status === 'pending template approval') ||
-        //       (p.status === 'rejected template')) {
-        //       this.serviceProviderService.getPendingServicesOfProvider(p.id).subscribe(
-        //         res => {
-        //           if (res && (res.length > 0)) {
-        //             this.pendingFirstServicePerProvider.push({providerId: p.id, serviceId: res[0].id});
-        //           }
-        //         }
-        //       );
-        //     }
-        //   }
-        // );
       }
     );
   }
 
-  showDeletionModal(resource: ServiceBundle) {
+  isProviderActive(id: string) {
+    let active = false;
+    for (let i = 0; this.providers[i]; i++ ) {
+      if (id === this.providers[i].id) {
+        active = this.providers[i].active;
+        break;
+      }
+    }
+    return active;
+  }
+
+  isProviderChecked(value: string) {
+    return this.dataForm.get('resource_organisation').value.includes(value);
+  }
+
+  onSelection(e, category: string, value: string) {
+    const formArrayNew: FormArray = this.dataForm.get('resource_organisation') as FormArray;
+    if (e.target.checked) {
+      this.addParameterToURL(category, value);
+      formArrayNew.push(new FormControl(value));
+    } else {
+      let categoryIndex = 0;
+      for (const urlParameter of this.urlParams) {
+        if (urlParameter.key === category) {
+          const valueIndex = urlParameter.values.indexOf(value, 0);
+          if (valueIndex > -1) {
+            urlParameter.values.splice(valueIndex, 1);
+            if (urlParameter.values.length === 0) {
+              this.urlParams.splice(categoryIndex, 1);
+            }
+          }
+          const formArrayIndex = formArrayNew.value.indexOf(value, 0);
+          if (formArrayIndex > -1 ) {
+            formArrayNew.removeAt(formArrayIndex);
+          }
+        }
+        categoryIndex++;
+      }
+    }
+    // this.getServices();
+    return this.navigateUsingParameters();
+  }
+
+  private addParameterToURL(category: string, value: string) {
+    let foundCategory = false;
+    for (const urlParameter of this.urlParams) {
+      if (urlParameter.key === category) {
+        foundCategory = true;
+        const valueIndex = urlParameter.values.indexOf(value, 0);
+        if (valueIndex < 0) {
+          urlParameter.values.push(value);
+          this.updatePagingURLParameters(0);
+        }
+      }
+    }
+    if (!foundCategory) {
+      this.updatePagingURLParameters(0);
+      const newParameter: URLParameter = {
+        key: category,
+        values: [value]
+      };
+      this.urlParams.push(newParameter);
+    }
+  }
+
+  navigateUsingParameters() {
+    const map: { [name: string]: string; } = {};
+    for (const urlParameter of this.urlParams) {
+      map[urlParameter.key] = urlParameter.values.join(',');
+    }
+    this.handleChange();
+    // return this.navigator.resourcesList(map);  // problematic semi-colon in url
+  }
+
+  updatePagingURLParameters(from: number) {
+    let foundFromCategory = false;
+    for (const urlParameter of this.urlParams) {
+      if (urlParameter.key === 'from') {
+        foundFromCategory = true;
+        urlParameter.values = [];
+        urlParameter.values.push(from + '');
+        break;
+      }
+    }
+    if (!foundFromCategory) {
+      const newFromParameter: URLParameter = {
+        key: 'from',
+        values: [from + '']
+      };
+      this.urlParams.push(newFromParameter);
+    }
+  }
+
+
+  showDeletionModal(resource: InfraService) {
     this.selectedService = resource;
     if (this.selectedService) {
       UIkit.modal('#deletionModal').show();
@@ -178,9 +305,7 @@ export class ResourcesListComponent implements OnInit {
     );
   }
 
-  toggleService(providerService: ServiceBundle) {
-    console.log('bundle');
-    console.log(providerService);
+  toggleService(providerService: InfraService) {
     UIkit.modal('#spinnerModal').show();
     this.serviceProviderService.publishService(providerService.id, providerService.service.version, !providerService.active).subscribe(
       res => {},
